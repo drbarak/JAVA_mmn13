c98d5b9693146b5283397d3008a5a12a7fb597cd
Zvi Barak <drbarak@talkie.co.il>
2024-11-29 22:54:01 +0200

    Add files via upload

--- ArrTest.java
+++ ArrTest.java
@@ -1,146 +1,88 @@
 import java.util.Scanner;
-import java.util.Arrays;
-
 public class ArrTest
 {
     // method to test the class
     public static void main(String[] args)
     {
-        //p("Enter 4 numbers, seperated by space for array with 4 numbers");
-        /*
-        Scanner scan = new Scanner(System.in);
-        int[] arr = new int[4];
-        arr[0] = scan.nextInt();
-        arr[1] = scan.nextInt();
-        arr[2] = scan.nextInt();
-        arr[3] = scan.nextInt();
-         */
-        /*
-        int[] arr = {4, -5, -3, 2, 1, 7, 0};
-        int[] newArr = Ex13.specialArr(arr, 10);
-        p("specialArr result=" + Arrays.toString(newArr));
-        int[] arr2 = {4, -5, -3, 2, 1, 7, 0, 9};
-        newArr = Ex13.specialArr(arr2, 10);
-        p("specialArr result=" + Arrays.toString(newArr));
-        int[] arr3 = {-6, -5, -3, 2, 1, 7, 0, -9};
-        newArr = Ex13.specialArr(arr3, 10);
-        p("specialArr result=" + Arrays.toString(newArr));
-         */
-        /*
-        int[] newArr = {4, -5, -3, 2, 1, 7, 0};
-        p("first +ve=" + Ex13.first(newArr) + ", array=" + Arrays.toString(newArr));
-        newArr = new int[]{1, -3, 6, 2, 0, 15};
-        p("first +ve=" + Ex13.first(newArr) + ", array=" + Arrays.toString(newArr));
-        newArr = new int[]{1, 1, 1, 1};
-        p("first +ve=" + Ex13.first(newArr) + ", array=" + Arrays.toString(newArr));
-        newArr = new int[]{0};
-        p("first +ve=" + Ex13.first(newArr) + ", array=" + Arrays.toString(newArr));
-        newArr = new int[]{1, 2, 3, 4};
-        p("first +ve=" + Ex13.first(newArr) + ", array=" + Arrays.toString(newArr));
-        newArr = new int[]{5, -1, 3, 1, 0, -2, 2};
-        p("first +ve=" + Ex13.first(newArr) + ", array=" + Arrays.toString(newArr));
-        newArr = new int[]{7,8,9,11,12,14};
-        p("first +ve=" + Ex13.first(newArr) + ", array=" + Arrays.toString(newArr));
-        */
-        /*
-        int fact = factorial(13);
-        p("fact = "+fact);
-         */
-        /*
-        int[] arr = {4, 10, 10,  3, 4};
-        arr = new int[] {4, 10, 10, 3, 10, 4};
-        arr = new int[] {4, 10, 10, 10, 3, 4};
-        arr = new int[] {4, 10, 10, 10, 10, 3, 10, 4};
-        arr = new int[] {10, 10, 4, 3, 10};
-        //    System.out.println("is Pila " + isSemiPalindropm(arr, -1, -1) + ", array=" + Arrays.toString(arr)); 
-
-        arr = new int[] {1, 1, 4, 10, 10, 4, 3, 10, 10};
-        //arr = new int[] {1, 1, 4};
-        p("is Pila " + Ex13.longestNearlyPal(arr) + ", array=" + Arrays.toString(arr));
-        */
-       int[][] m = {{-1, -2, -3}, {-4, -5, -6}, {-7, -8, -9}};
-       p("extreme " + Ex13.extreme(m));
-       for (int row = 0; row < m.length; row++)
-           p(Arrays.toString(m[row]));
-/*           
-       char[][] c = {{'X',   'O',   'X'},   {'O',   'O',   'X'},   {'X',   'X',   'O'}};
-       for (int row = 0; row < c.length; row++)
-           p(Arrays.toString(c[row]));
-           */
-       /*
-       for (int i = 0; i < m[0].length; i++)
-       {
-           p("" + m[0][i] + ", " +  m[1][i]);
-       }
-       */
-       final int RIGHT_DOWN = 1;
-       final int LEFT_UP = -1;
-        final int POINT1 = 0;
-        final int POINT2 = 1;
-
-       int row = 0;
-         while (row < m[0].length)
-         {
-             int j = 0;
-             while (j < m[1].length)
-             {
-                 p(row, j, m[row][j]);
-                 int[] pt = {row, j};
-                 int[][] next2Pts = next2Points(pt, m[0].length);
-                 p(Arrays.toString(next2Pts[POINT1]));
-                 p(Arrays.toString(next2Pts[POINT2]));
-                 j += RIGHT_DOWN;
-             }
-             row += RIGHT_DOWN;
-         }
-         int[] endPoint = {m[0].length - 1, m[0].length -1};
-         nextPointLeft(endPoint);
-         nextPointUp(endPoint);
-    }
-        final static int ROW = 0;
-        final static int COLUMN = 1;
-    private static int[][] next2Points(int[] point, int endPoint)
-    {
-        int[][] next2Points = new int[2][2];
-        final int POINT1 = 0;
-        final int POINT2 = 1;
-        next2Points[POINT1][ROW] = point[ROW];
-        if (point[COLUMN] + 1 < endPoint) // move right
-            next2Points[POINT1][COLUMN] = point[COLUMN] + 1;
-        else
-            next2Points[POINT1][COLUMN] = -1;
-        next2Points[POINT2][COLUMN] = point[COLUMN];
-        if (point[ROW] + 1 < endPoint) // move down
-            next2Points[POINT2][ROW] = point[ROW] + 1;
-        else
-            next2Points[POINT2][ROW] = -1;
-        return next2Points;
-    }
-    private static int[] nextPointLeft(int[] point)
-    {
-        int[] newPoint = {point[ROW], point[COLUMN]};
-        p(Arrays.toString(point));
-        if (point[ROW] <= 0)
+        int run = 1;//1000+100+10+1;
+        int[][] m;
+        int[] arr;
+        boolean p = true;
+        if (run >= 1000)
         {
-            newPoint[ROW] = -1;
-            return newPoint;
+            m = new int[][]{{1, 3}, {4, 2}};
+            m = new int[][]{{1, 2}, {3, 4}};
+            m = new int[][]{{4,5,8,2}, {3,12,7,16}, {13,1,10,14}, {15,11,9,6}};
+            m = new int[][]{{4,5,8,2}, {3,12,16, 7}, {13,1,10,14}, {15,11,9,6}};
+            m = new int[][]{{4,1,9,3,25}, {24,23,22,21,5}, {13,12,15,16,14}, {17,11,18,19,20}, {10,2,8,7,6}};
+            m = new int[][]{{4,1,9,3,25}, {24,23,2,21,5}, {13,12,15,16,22}, {17,11,18,19,20}, {10,14,8,7,6}};
+            m = new int[][]{{-1, -2, -3}, {-4, -5, -6}, {-7, -8, -9}};
+            Print.p("------ extreme ------");
+            for (int row = 0; row < m.length; row++)
+                Print.p("row " + row, m[row]);
+            Print.p("extreme minMax = " + Ex13.extreme(m, false));
         }
-        nextPointUp(newPoint);
-        newPoint[ROW]--;
-        return nextPointLeft(newPoint);
-    }
-    private static int[] nextPointUp(int[] point)
-    {
-        int[] newPoint = {point[ROW], point[COLUMN]};
-        p(Arrays.toString(point));
-        if (point[COLUMN] <= 0)
+        if (run % 1000 >= 100)
         {
-            newPoint[COLUMN] = -1;
-            return newPoint;
+            Print.p("\n------ specialArr ------");
+            arr = new int[]{4, -5, -3, 2, 1, 7, 0};
+            Print.p("Result=", Ex13.specialArr(arr, 1));
+            arr = new int[]{4, -5, -3, 2, 1, 7, 0, 9};
+            Print.p("Result=", Ex13.specialArr(arr, 10));
+            arr = new int[]{-6, -5, -3, 2, 1, 7, 0, -9};
+            Print.p("Result=", Ex13.specialArr(arr, 10));
         }
-        newPoint[COLUMN]--;
-        return nextPointUp(newPoint);
+        if (run % 100 >= 10)
+        {
+            Print.p("\n------ first ------");
+            arr = new int[]{4, -5, -3, 2, 1, 7, 0};
+            Print.p("first +ve=" + Ex13.first(arr) + ", array=", arr);
+            arr = new int[]{1, -3, 6, 2, 0, 15};
+            Print.p("first +ve=" + Ex13.first(arr) + ", array=", arr);
+            arr = new int[]{1, 1, 1, 1};
+            Print.p("first +ve=" + Ex13.first(arr) + ", array=", arr);
+            arr = new int[]{0};
+            Print.p("first +ve=" + Ex13.first(arr) + ", array=", arr);
+            arr = new int[]{1, 2, 3, 4};
+            Print.p("first +ve=" + Ex13.first(arr) + ", array=", arr);
+            arr = new int[]{5, -1, 3, 1, 0, -2, 2};
+            Print.p("first +ve=" + Ex13.first(arr) + ", array=", arr);
+            arr = new int[]{7,8,9,11,12,14};
+            Print.p("first +ve=" + Ex13.first(arr) + ", array=", arr);
+        }
+        //if (run % 1000 >= 10)
+        if (run % 10 > 0)
+        {
+            Print.p("\n------ isSemiPalindropm ------");
+            arr = new int[] {4, 10, 10,  3, 4};
+            Print.p("is Pila " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {4, 10, 10, 3, 10, 4};
+            Print.p("is Pila " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {4, 10, 10, 10, 3, 4};
+            Print.p("is Pila " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {4, 10, 10, 10, 10, 3, 10, 4};
+            Print.p("is Pila " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {10, 10, 4, 3, 10};
+            Print.p("is Pila " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {-1, -1};
+            Print.p("is Pila " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {1, 1, 4, 10, 10, 4, 3, 10, 10};
+            Print.p("is Pila " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {1, 1, 4};
+            Print.p("is Pila " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {4, 2, 2};
+            Print.p("is Pila " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {2, 4, 2};
+            Print.p("is Pila " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {};
+            Print.p("is Pila " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+        }
     }
+
+    /*
+    int fact = factorial(13);
+    Print.p("fact = "+fact);
+     */
 
     private static int factorial(int n)
     {
@@ -148,22 +90,71 @@ public class ArrTest
             return n * factorial(n -1);
         return 1;
     }
-
-    private static void p(String s)
-    {
-        System.out.println(s);
-    }
-
-    private static void p(int s)
-    {
-        p(" " + s);
-    }
-    private static void p(int n1, int n2)
-    {
-        p("" + n1 + ", " + n2);
-    }
-    private static void p(int n1, int n2, int n3)
-    {
-        p("" + n1 + ", " + n2 + ", " + n3);
-    }
 }
+/* test numOfPaths
+//p(numOfPaths(1, 1));
+//int n = numOfPaths(SIZE, SIZE);
+//int[] arr = new int[n];
+//numOfPaths(_size, _size);
+//p("Enter 4 numbers, seperated by space for array with 4 numbers");
+ * 
+private static int _size = 2;
+private static int _count = 0;
+private static int _route = 0;
+private static int[] lastX = new int[100], lastY = new int[100];
+private static int numOfPaths(int x, int y, int _route)
+{
+Print.p(x, y, _count);
+if (x == 0 && y == 0) return 0;
+if (x != 0 && y != 0)
+{
+if (p) Print.p("_route=" + _route + ", x=" + 
+x + ", y=" + y + ", lastX=" + lastX[_route] + ", lastY=" + lastY[_route]);
+lastX[_route] = x;
+lastY[_route] = y;
+_route++;
+}
+if (p) Print.p("_route2=" + _route);
+//        if (x == 0 || y == 0) return 1;
+//if (x == _size && y == _size) return 0;
+//if (x < 0 || y < 0 || x > MAX || y > MAX) return 0;
+if (x > 0) 
+{
+if (p) Print.p("calling 1");
+numOfPaths(x - 1, y, _route);    // left
+if (x - 1 == 0 && y == 0) _count++;
+if (p) Print.p("returning 1");
+}
+if (x < _size) 
+{
+if (p) Print.p("before calling 3 _route=" + (_route - 1) + ", x=" + 
+x + ", y=" + y + ", lastX=" + lastX[_route - 1] + ", lastY=" + lastY[_route - 1]);
+if (x != lastX[_route - 1] && y != lastY[_route - 1])
+{
+if (p) Print.p("calling 3");
+numOfPaths(x + 1, y, _route);    // right
+if (x + 1 == 0 && y == 0) _count++;
+if (p) Print.p("returning 3");
+}
+}
+if (y > 0)
+{
+if (p) Print.p("calling 2");
+numOfPaths(x, y - 1, _route);    // down
+if (x == 0 && y - 1 == 0) _count++;
+if (p) Print.p("returning 2");
+
+}
+//if (x < _size) numOfPaths(x + 1, y);  // right
+//if (y < MAX) numOfPaths(x, y + 1);    // up
+Print.p("_count = " + _count);
+return 0;
+}
+private static int numOfPathsOrg(int x, int y)
+{
+if (x == 0 && y == 0) return 0;
+if (x == 0 || y == 0) return 1;
+return numOfPathsOrg(x - 1, y) + numOfPathsOrg(x, y - 1);
+}
+ */
+
--- Ex13.java
+++ Ex13.java
@@ -14,72 +14,136 @@ public class Ex13
      *
      * @param    int[] arr - the array to arrange
      * @param    int med - the median of the array
-     * @return   int[] specialArr - the special arry
+     * @return   int[] specialArr - the special array
      */
     public static int[] specialArr(int[] arr, int med)
     {
-        p("org array: length=" + arr.length + ", "+ Arrays.toString(arr));
-        int[] workArr = sortArr(arr);  // copy the original array so not to change it
+        Print.p("org array: length=" + arr.length + ", "+ Arrays.toString(arr));
+        int[] workArr = sortArr(arr);  // copy and sort the original array so not to change it
         // find the median and compare to input parameters
         int calcMed;
         calcMed = workArr[arr.length / 2];
-        p("Special array: median is " + calcMed + ", " + Arrays.toString(workArr));        
+        Print.p("Special array: median is " + calcMed + ", " + Arrays.toString(workArr));        
         if (med != calcMed)
-            p("Provided med = " + med + " is wrong, using calcMed = " + calcMed);
+            Print.p("Provided med = " + med + " is wrong, using calcMed = " + calcMed);
         int[] specialArr = new int[arr.length];
         specialArr[0] = calcMed;
-        int index = 1;
-        for (int i = 1; i < arr.length; i++)
-        {
-            if (i % 2 == 1) // index odd have a number smaller than its' neighbours
-                specialArr[i] = workArr[index - 1]; 
-            else
-                specialArr[i] = workArr[arr.length - index++]; 
-        }
+        special(specialArr, workArr, 1, 1);
         return specialArr;
     }
+
+    private static void special(int [] specialArr, int [] workArr, int i, int index)
+    {
+        if (i == workArr.length) return;
+        if (i % 2 == 1) // index odd have a number smaller than its' neighbours
+            specialArr[i] = workArr[index - 1]; 
+        else
+            specialArr[i] = workArr[workArr.length - index++]; 
+        special(specialArr, workArr, ++i, index);
+    }
+    private static final int INVALID_NUM = -1;
+    private static int nextNum;
     public static int first(int [] arr)
     {
-        final int INVALID_NUM = -1;
         int[] workArr = sortArr(arr);  // copy the original array so not to change it
-        int nextNum = INVALID_NUM;
-        for (int num: workArr)
-        {
-            if (num < 1) continue;
-            if (nextNum < num)
-            {
-                if (nextNum == INVALID_NUM)
-                {
-                    if (num > 1)
-                        return 1;
-                }
-                else if (num - nextNum > 1)
-                    return nextNum + 1;
-                nextNum = num;
-            }
-        }
-       return (nextNum == INVALID_NUM ? 1 : nextNum + 1);
+        if (p) Print.p(workArr);
+        nextNum = INVALID_NUM;
+        firstNum(workArr, 0);
+        return (nextNum == INVALID_NUM ? 1 : nextNum + 1);
     }
+
+    private static boolean firstNum(int [] workArr, int i)
+    {
+        if (i == workArr.length) return true;
+        int num = workArr[i];
+        if (p) Print.p(num, i, nextNum);
+        if (num < 1) return firstNum(workArr, ++i);  // skip 0 and -ve numbers
+        if (nextNum < num)
+        {
+            if (nextNum == INVALID_NUM)
+            {
+                if (num > 1)
+                    return firstNum(workArr, workArr.length);
+            }
+            else if (num - nextNum > 1)
+                return firstNum(workArr, workArr.length);
+            nextNum = num;
+        }
+        return firstNum(workArr, ++i);
+    }
+    static boolean p = false;
     public static int longestNearlyPal(int[] arr)
     {
+        // sub arrays from pos i to end
+        if (p) Print.p(arr);
+        return _isSemiPalindrom(arr, true, 0, 0);
+        /*      
         for (int i = 0; i < arr.length; i++)
         {
-            int[] arr2 = new int[arr.length - i];
-            for (int k = 0; k < arr.length - arr2.length + 1; k++)
-            {
-                for (int j = 0; j < arr2.length; j++)
-                    arr2[j] = arr[j + k];
-                boolean r = isSemiPalindrom(arr2);
-                if (r)
-                    return arr2.length;
-            }
+        int[] arr2 = new int[arr.length - i];
+        // sub-sub arrays from pos 0 to end-k
+        for (int k = 0; k < arr.length - arr2.length + 1; k++)
+        {
+        // arraycopy(source_arr, int sourcePos, dest_arr, int destPos, int len)
+        System.arraycopy(arr, k, arr2, 0, arr2.length);
+        //for (int j = 0; j < arr2.length; j++) arr2[j] = arr[j + k];
+        Print.p(i, k);
+        Print.p(arr2);
+        boolean r = _isSemiPalindrom(arr2, true);
+        if (false && r)
+        {
+        if(p) Print.p(k);
+        return arr2.length;
+        }
+        }
         }
         return 0;
+         */
     }
-    private static boolean isSemiPalindrom(int[] arr)
+
+    private static int _isSemiPalindrom(int[] arr, boolean internal, int i, int kk)
     {
-        // call the method flaging that a number at the edge can not be dropped
-        return _isSemiPalindrom(arr, true);
+        if (kk == i + 1)
+        {
+            i++;
+            kk = 0;
+        }
+        if (i == arr.length) return 0;
+        if (p) Print.p(i, kk);
+        int[] arr2 = new int[arr.length - i];
+        System.arraycopy(arr, kk, arr2, 0, arr2.length);
+        if (p) Print.p(arr2);
+        boolean rr = _isSemiPalindrom(arr2, true);
+        if (p) Print.p(rr);
+        if (rr)
+        {
+            if (p) Print.p(arr2.length);
+            kk = arr.length;
+            i = arr.length;
+            return arr2.length;
+        }
+        if (true) return _isSemiPalindrom(arr, true, i, ++kk);
+        for (int k = 0; k < arr.length - arr2.length + 1; k++)
+        {
+            /*
+             * arraycopy(source_arr, int sourcePos,
+            dest_arr, int destPos, int len)
+             */
+            System.arraycopy(arr, k, arr2, 0, arr2.length);
+            /*
+            for (int j = 0; j < arr2.length; j++)
+            arr2[j] = arr[j + k];
+             */
+            Print.p(i, k);
+            Print.p(arr2);
+            boolean r = _isSemiPalindrom(arr2, true);
+            if (false && r)
+            {
+                if(p) Print.p(k);
+                //return arr2.length;
+            }
+        }
+        return -3;
     }
     private static boolean _isSemiPalindrom(int[] arr, boolean internal)
     {
@@ -96,13 +160,20 @@ public class Ex13
                 {
                     // drop the left number of the pair of digits that are not equal and check again
                     int[] arr2 = new int[arr.length-2*i-1];
+                    System.arraycopy(arr, i + 1, arr2, 0, arr2.length);
+                    /*
                     for (int j = 0; j < arr2.length; j++)
-                        arr2[j] = arr[i + j + 1];
+                    arr2[j] = arr[i + j + 1];
+                    Print.p(arr2);
+                     */
                     boolean r = _isSemiPalindrom(arr2, false);
                     if (!r) // now drop the right digit of the pair and check again
                     {
+                        System.arraycopy(arr, i, arr2, 0, arr2.length);
+                        /*
                         for (int j = 0; j < arr2.length; j++)
-                            arr2[j] = arr[i + j];
+                        arr2[j] = arr[i + j];
+                         */
                         r = _isSemiPalindrom(arr2, false);
                     }
                     if (!r) return false;
@@ -113,38 +184,78 @@ public class Ex13
         }
         return true;
     }
-    
-    public static int extreme(int[][] mat)
+    private static int minMax = Integer.MAX_VALUE;
+    private static int count = 0;
+    public static int extreme(int[][] mat, boolean _p)
     {
-        int n = mat.length;
-        //p(n + " " + mat[0].length + ", " + mat[1].length);
-        return 0;
+        p = _p;
+        findPaths(mat.length - 1, mat.length - 1, "", -1, mat);
+        return minMax;
     }
 
-            // sort an array of integers - using 'selection sort'
+    private static void findPaths(int x, int y, String path, int max, int[][] m)
+    {
+        String point = makePt(x, y);
+        path += point;
+        if (p) Print.p(path + ",max=" + max + ",value=" + m[x][y]);
+        if (max < m[x][y]) max = m[x][y];
+        if (x == 0 && y == 0)
+        {
+            if (minMax > max) minMax = max;
+            if (p) Print.p("Path = [" + path + "] max=" + max + ", minMax=" + minMax + ", count=" + ++count);
+            return;
+        }
+        if (x > 0 && path.indexOf(makePt(x - 1, y)) < 0) // prevents left after right because it returns to prev point
+        {
+            if (p) Print.p("calling L " + point);
+            findPaths(x - 1, y, path + ",", max, m);    // left
+            if (p) Print.p("returning L " + point);
+        }
+        if (x < m.length - 1 && path.indexOf(makePt(x + 1, y)) < 0) 
+        {
+            if (p) Print.p("calling R " + point);
+            findPaths(x + 1, y, path + ",", max, m);     // right
+            if (p) Print.p("returning R " + point);
+        }
+        if (y > 0 && path.indexOf(makePt(x, y - 1)) < 0)
+        {
+            if (p) Print.p("calling D " + point);
+            findPaths(x, y - 1, path + ",", max, m);    // down
+            if (p) Print.p("returning D " + point);
+
+        }
+        if (false && y < m.length - 1 && path.indexOf(makePt(x, y + 1)) < 0) 
+        {
+            if (p) Print.p("calling U " + point);
+            findPaths(x, y + 1, path + ",", max, m);     // right
+            if (p) Print.p("returning U " + point);
+        }
+    }
+
+    private static String makePt(int x, int y)
+    {
+        return "(" + x + "," + y + ")";
+    }
+
     private static int[] sortArr(int[] arr)
     {
         int[] workArr = arr.clone();  // copy the original array so not to change it
+        Arrays.sort(workArr);
+        // sort an array of integers - using 'selection sort'
+        /*
         for (int i = 0; i < arr.length; i++)
         {
-            int minIndex = i;
-            for (int j = i + 1; j < arr.length; j++)
-            {
-                if (workArr[j] < workArr[minIndex])
-                    minIndex = j;
-            }
-            int temp = workArr[i];
-            workArr[i] = workArr[minIndex];
-            workArr[minIndex] = temp; 
+        int minIndex = i;
+        for (int j = i + 1; j < arr.length; j++)
+        {
+        if (workArr[j] < workArr[minIndex])
+        minIndex = j;
         }
+        int temp = workArr[i];
+        workArr[i] = workArr[minIndex];
+        workArr[minIndex] = temp; 
+        }
+         */
         return workArr;
-    }
-    private static void p(String s)
-    {
-        System.out.println(s);
-    }
-    private static void p(int n)
-    {
-        p("" + n);
     }
 }
