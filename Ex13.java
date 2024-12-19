b23980d14178688f7f91360382e71bcb6507d595
Zvi Barak <drbarak@talkie.co.il>
2024-12-19 07:58:24 +0200

    Add files via upload
    Before making changes due to clarification of the project (eg/ not to use an extra array in the method SpecialArr())

--- Ex13.java
+++ Ex13.java
@@ -6,34 +6,160 @@
  * @version (13.11.2024)
  */
 import java.util.Arrays;
-
 public class Ex13
 {
     /**
-     * A method - accepts an array of whole numbers and return it as a special arry
+     * A method - accepts an array of whole numbers and return it as a special array
      *
      * @param    int[] arr - the array to arrange
      * @param    int med - the median of the array
      * @return   int[] specialArr - the special array
      */
+    static boolean p = false;
+    static int count = 0;
+    private static int calcMedian(int[] org,int[] a)
+    {
+        int median = org[0];
+        int countLess = 0, countMore = 0;
+        for (int i=1; i<org.length; i++)
+        {
+            count++;
+            int curr = org[i];
+            int maxCountLess = (i+1)/2;
+            int maxCountMore = ((i+1) % 2 == 1 ? maxCountLess : maxCountLess - 1);
+            if (p) Print.p("i=" + i + ",median="+median+ ",curr="+curr);
+            if (p) Print.p("100, countLess="+countLess+",maxCountLess="+maxCountLess+
+                ",countMore="+countMore+",maxCountMore=" + maxCountMore);
+            if (curr < median)
+            {
+                if (p) Print.p("500, curr < median");
+                if (countLess == maxCountLess) // can't add to Less list
+                {
+                    boolean found = false;
+                    int maxIndex = countLess;
+                    if (p) Print.p(1000, curr, median, maxIndex-1);
+                    // find if in the Less list a number larger than curr then 
+                    // median is added to the Less list and curr become the median
+                    for (int j=countLess;j>0;j--)
+                    {   // if found replace it in the Less list and use it as a median
+                        // and add the median to the More list
+                        count++;
+                        if (a[j-1] > curr)
+                        {
+                            a[org.length - countMore - 1] = median;
+                            countMore++;
+                            median = a[j-1];
+                            a[j-1] = curr;
+                            found = true;
+                            if (p) Print.p("found");
+                            break;
+                        }
+                        else if (a[maxIndex-1] < a[j-1])
+                            maxIndex = j-1;
+                    }
+                    if (!found)
+                    {
+                        if (p) Print.p("not found,"+maxIndex);
+                        a[org.length - countMore - 1] = median;
+                        countMore++;
+                        median = curr;
+                    }
+                }
+                else
+                {
+                    a[countLess] = curr;
+                    countLess++;
+                }
+            }
+            else
+            {
+                if (countMore == maxCountMore) // can't add to Larger list
+                {
+                    boolean found = false;
+                    int minIndex = org.length - countMore;
+                    if (p) Print.p(2000, curr, median, minIndex);
+                    // find if in the More list a number less than curr then 
+                    // median is added to the Less list and curr become the median
+                    for (int j=countMore;j>0;j--)
+                    {   // if found, add median it to the Less list and curr becomes the median
+                        count++;
+                        if (a[org.length - j] > curr)
+                        {
+                            a[countLess] = median;
+                            countLess++;
+                            median = curr;
+                            found = true;
+                            if (p) Print.p("found");
+                            break;
+                        }
+                        else if (a[minIndex] > a[org.length - j])
+                            minIndex = org.length - j;
+                    }
+                    if (!found)
+                    {
+                        // not found so replace curr with the minimum position in the
+                        // More list, add median to the Less list and the minimum as median
+                        if (p) Print.p("not found,"+minIndex);
+                        a[countLess] = median;
+                        countLess++;
+                        if (countMore > 0)
+                        {
+                            median = a[minIndex];
+                            a[minIndex] = curr; 
+                        }
+                        else
+                            median = curr;
+                    }
+                }
+                else
+                {
+                    a[org.length - countMore - 1] = curr;
+                    countMore++;
+                }
+            }
+            if (p) Print.p("i=" + i + ",median="+median+ ", "+ Arrays.toString(a));
+            if (p) Print.p("countLess="+countLess+",maxCountLess="+maxCountLess+
+                ",countMore="+countMore+",maxCountMore=" + maxCountMore);
+            //if (i == 6) break;
+        }
+        a[countLess] = median;
+        return median;
+    }
+    public static int[] specialArrNoSort(int[] arr, int med)
+    {
+        count = 0;
+        if (p) Print.p("org array: length=" + arr.length + ", "+ Arrays.toString(arr));
+        int[] a = new int [arr.length];
+        int calcMed = calcMedian(arr, a);
+        if (p) Print.p("Calculated median is "+calcMed + ", "+ Arrays.toString(a));
+        int[] specialArr = new int[arr.length];
+        specialArr[0] = calcMed;
+        special(specialArr, a, 1, 1);
+        Print.p("count=" + count);
+        Print.p("Result=", specialArr);
+        return specialArr;
+    }
     public static int[] specialArr(int[] arr, int med)
     {
-        Print.p("org array: length=" + arr.length + ", "+ Arrays.toString(arr));
+        count = 0;
+        specialArrNoSort(arr, med);
+        count = 0;
         int[] workArr = sortArr(arr);  // copy and sort the original array so not to change it
         // find the median and compare to input parameters
         int calcMed;
         calcMed = workArr[arr.length / 2];
-        Print.p("Special array: median is " + calcMed + ", " + Arrays.toString(workArr));        
+        if (p) Print.p("Special array: median is " + calcMed + ", " + Arrays.toString(workArr));        
         if (med != calcMed)
-            Print.p("Provided med = " + med + " is wrong, using calcMed = " + calcMed);
+            if (p) Print.p("Provided med = " + med + " is wrong, using calcMed = " + calcMed);
         int[] specialArr = new int[arr.length];
         specialArr[0] = calcMed;
         special(specialArr, workArr, 1, 1);
+        Print.p("count=" + count);
         return specialArr;
     }
-
     private static void special(int [] specialArr, int [] workArr, int i, int index)
     {
+        count++;
         if (i == workArr.length) return;
         if (i % 2 == 1) // index odd have a number smaller than its' neighbours
             specialArr[i] = workArr[index - 1]; 
@@ -51,7 +177,6 @@ public class Ex13
         firstNum(workArr, 0);
         return (nextNum == INVALID_NUM ? 1 : nextNum + 1);
     }
-
     private static boolean firstNum(int [] workArr, int i)
     {
         if (i == workArr.length) return true;
@@ -71,44 +196,64 @@ public class Ex13
         }
         return firstNum(workArr, ++i);
     }
-    static boolean p = false;
     public static int longestNearlyPal(int[] arr)
     {
-        if (p) Print.p(arr);
-        return _isSemiPalindrom(arr, true, 0, 0);
+        return isSemiPalindrome(arr, true, 0, 0);
     }
-
-    private static int _isSemiPalindrom(int[] arr, boolean internal, int i, int kk)
+    private static int isSemiPalindrome(int[] arr, boolean internal, int i, int k)
     {
-        if (kk == i + 1)
+        if (p) Print.p("in method 1, i=" + i + ", k=" + k, arr);
+        if (k == i + 1)
         {
             i++;
-            kk = 0;
+            k = 0;
         }
-        if (i == arr.length) return 0;
-        if (p) Print.p("in method 1, i=" + i, kk);
-        int[] arr2 = new int[arr.length - i];
-        System.arraycopy(arr, kk, arr2, 0, arr2.length);
-        if (p) Print.p(arr2);
-        boolean rr = _isSemiPalindrom(arr2, true);
-        if (p) Print.p(rr);
-        if (rr)
+        int len2 = arr.length - i;
+        if (len2 < 2)
         {
-            if (p) Print.p(arr2.length);
-            kk = arr.length;
-            i = arr.length;
-            return arr2.length;
+            k = i = arr.length;
+            return len2;
         }
-        return _isSemiPalindrom(arr, true, i, ++kk);
+        int[] arr2 = new int[len2];
+        System.arraycopy(arr, k, arr2, 0, len2);
+        if (isSemiPalindrome(arr2, true, 0))
+        {
+            k = i = arr.length;
+            if (p) Print.p("end ", arr2);
+            return len2;
+        }
+        return isSemiPalindrome(arr, true, i, ++k);
     }
-    private static boolean _isSemiPalindrom(int[] arr, boolean internal)
+    // same as above but with extra debugging prints
+    private static int __isSemiPalindrome(int[] arr, boolean internal, int i, int k)
     {
-        if (p) Print.p("in method 2 ", arr);
-        if (arr.length < 2) return true;    // a single number is Palindrom so no need to check
-        // loop over all possibles sub-arrays of original array
-        return _isSemiPalindrom(arr, internal, 0);
+        if (p) Print.p("in method 1, i=" + i + ", k=" + k, arr);
+        if (k == i + 1)
+        {
+            i++;
+            k = 0;
+        }
+        //if (i == arr.length) return 0;
+        int len2 = arr.length - i;
+        if (len2 < 2)
+        {
+            k = i = arr.length;
+            return len2;
+        }
+        int[] arr2 = new int[len2];
+        System.arraycopy(arr, k, arr2, 0, len2);
+        if (p) Print.p(arr2);
+        if (isSemiPalindrome(arr2, true, 0))
+        {
+            if (p) Print.p(true);
+            if (p) Print.p(len2);
+            k = i = arr.length;
+            return len2;
+        }
+        if (p) Print.p(false);
+        return isSemiPalindrome(arr, true, i, ++k);
     }
-    private static boolean _isSemiPalindrom(int[] arr, boolean internal, int i)
+    private static boolean isSemiPalindrome(int[] arr, boolean internal, int i)
     {
         if (p) Print.p("in method 3, i=" + i + " ", arr);
         if (i == arr.length / 2) return true;
@@ -116,34 +261,36 @@ public class Ex13
         // the position of the digits is shifted by the index i
         if (arr[i] != arr[arr.length - 1 - i])
         {
-            // remove one number, if the array  is internal to the original and check again
+            // remove one number, if the array is internal to the original and check again
             if (i > 0 && internal)
             {
+                // this code checks for SemiPalindrome - if it is removed then only true Palindrome is valid
                 // drop the left number of the pair of digits that are not equal and check again
-                int[] arr2 = new int[arr.length-2*i-1];
-                System.arraycopy(arr, i + 1, arr2, 0, arr2.length);
-                boolean r = _isSemiPalindrom(arr2, false);
-                if (!r) // now drop the right digit of the pair and check again
+                int len2 = arr.length-2*i-1;
+                if (len2 > 1)
                 {
-                    System.arraycopy(arr, i, arr2, 0, arr2.length);
-                    r = _isSemiPalindrom(arr2, false);
+                    int[] arr2 = new int[len2];
+                    System.arraycopy(arr, i + 1, arr2, 0, len2);
+                    boolean r = isSemiPalindrome(arr2, false, 0);
+                    if (!r) // now drop the right digit of the pair and check again
+                    {
+                        System.arraycopy(arr, i, arr2, 0, len2);
+                        r = isSemiPalindrome(arr2, false, 0);
+                    }
+                    if (!r) return false;
                 }
-                if (!r) return false;
             }
             else
                 return false;
         }
-        return _isSemiPalindrom(arr, internal, ++i);
+        return isSemiPalindrome(arr, internal, ++i);
     }
     private static int minMax = Integer.MAX_VALUE;
-    private static int count = 0;
-    public static int extreme(int[][] mat, boolean _p)
+    public static int extreme(int[][] mat)
     {
-        p = _p;
         findPaths(mat.length - 1, mat.length - 1, "", -1, mat);
         return minMax;
     }
-
     private static void findPaths(int x, int y, String path, int max, int[][] m)
     {
         String point = makePt(x, y);
@@ -191,22 +338,32 @@ public class Ex13
     private static int[] sortArr(int[] arr)
     {
         int[] workArr = arr.clone();  // copy the original array so not to change it
-        Arrays.sort(workArr);
+        //Arrays.sort(workArr);
         // sort an array of integers - using 'selection sort'
-        /*
         for (int i = 0; i < arr.length; i++)
         {
-        int minIndex = i;
-        for (int j = i + 1; j < arr.length; j++)
-        {
-        if (workArr[j] < workArr[minIndex])
-        minIndex = j;
+            count++;
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++)
+            {
+                count++;
+                if (workArr[j] < workArr[minIndex])
+                    minIndex = j;
+            }
+            swap(workArr, i, minIndex);
+            /*
+            int temp = workArr[i];
+            workArr[i] = workArr[minIndex];
+            workArr[minIndex] = temp; 
+            */
         }
-        int temp = workArr[i];
-        workArr[i] = workArr[minIndex];
-        workArr[minIndex] = temp; 
-        }
-         */
         return workArr;
+    }
+    private static void swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp; 
+    
     }
 }
---
+++ Tester.java
@@ -0,0 +1,168 @@
+public class Tester
+{
+    // method to test the class
+    public static void main()
+    {
+        int[] run = {0,0,1,0};
+        int[][] m;
+        int[] arr;
+        boolean p = true;
+        if (run[3] == 1)
+        {
+            m = new int[][]{{1, 3}, {4, 2}};
+            m = new int[][]{{1, 2}, {3, 4}};
+            m = new int[][]{{4,5,8,2}, {3,12,7,16}, {13,1,10,14}, {15,11,9,6}};
+            m = new int[][]{{4,5,8,2}, {3,12,16, 7}, {13,1,10,14}, {15,11,9,6}};
+            m = new int[][]{{4,1,9,3,25}, {24,23,22,21,5}, {13,12,15,16,14}, {17,11,18,19,20}, {10,2,8,7,6}};
+            m = new int[][]{{4,1,9,3,25}, {24,23,2,21,5}, {13,12,15,16,22}, {17,11,18,19,20}, {10,14,8,7,6}};
+            m = new int[][]{{-1, -2, -3}, {-4, -5, -6}, {-7, -8, -9}};
+            Print.p("------ extreme ------");
+            for (int row = 0; row < m.length; row++)
+                Print.p("row " + row, m[row]);
+            Print.p("extreme minMax = " + Ex13.extreme(m));
+        }
+        if (run[2] == 1)
+        {
+            Print.p("\n------ specialArr ------");
+            arr = new int[]{4, -5, -3, 1, 2, 7, 0};
+            arr = new int[]{2, -5, -3, 1, 4, 7, 0};
+            Print.p("Result=", Ex13.specialArr(arr, 1));
+            /*
+            arr = new int[]{4, -5, -3, 2, 1, 7, 0, 9};
+            Print.p("Result=", Ex13.specialArr(arr, 10));
+            arr = new int[]{-6, -5, -3, 2, 1, 7, 0, -9};
+            Print.p("Result=", Ex13.specialArr(arr, 10));
+        arr = new int[]{-5, 1, -3, 2, -2, 7, 0};
+        arr = new int[]{7, 6, 5, 4, 3, 2, 1, 8, 9, 10, 11, 12,13,14,15,16,17};
+        arr = new int[]{1,2,3,4,5,6,7, 8, 9, 10, 11, 12,13,14,15,16,17,19,20,21,22,23,24,25};
+            
+            */
+        }
+        if (run[1] == 1)
+        {
+            Print.p("\n------ first ------");
+            arr = new int[]{4, -5, -3, 2, 1, 7, 0};
+            Print.p("first +ve=" + Ex13.first(arr) + ", array=", arr);
+            arr = new int[]{1, -3, 6, 2, 0, 15};
+            Print.p("first +ve=" + Ex13.first(arr) + ", array=", arr);
+            arr = new int[]{1, 1, 1, 1};
+            Print.p("first +ve=" + Ex13.first(arr) + ", array=", arr);
+            arr = new int[]{0};
+            Print.p("first +ve=" + Ex13.first(arr) + ", array=", arr);
+            arr = new int[]{1, 2, 3, 4};
+            Print.p("first +ve=" + Ex13.first(arr) + ", array=", arr);
+            arr = new int[]{5, -1, 3, 1, 0, -2, 2};
+            Print.p("first +ve=" + Ex13.first(arr) + ", array=", arr);
+            arr = new int[]{7,8,9,11,12,14};
+            Print.p("first +ve=" + Ex13.first(arr) + ", array=", arr);
+        }
+        if (run[0] == 1)
+        {
+            Print.p("\n------ isSemiPalindrome ------");
+            arr = new int[] {1, 2, 3, 4, 5};
+            /**/
+            Print.p("isSemiPalindrome " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {1, 2, 3, 5, 5};
+            Print.p("isSemiPalindrome " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {1, 2, 3, 4, 4};
+            Print.p("isSemiPalindrome " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {1, 2, 3, 3, 5};
+            Print.p("isSemiPalindrome " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {1, 3, 2, 3, 5};
+            Print.p("isSemiPalindrome " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {4, 10, 10, 3, 4};
+            Print.p("isSemiPalindrome " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {4, 10, 10, 3, 10, 4};
+            Print.p("isSemiPalindrome " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {4, 10, 10, 10, 3, 4};
+            Print.p("isSemiPalindrome " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {4, 10, 10, 10, 10, 3, 10, 4};
+            Print.p("isSemiPalindrome " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {10, 10, 4, 3, 10};
+            Print.p("isSemiPalindrome " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            /**/
+            arr = new int[] {1, 1, 4, 10, 10, 4, 3, 10, 10};
+            Print.p("isSemiPalindrome " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            /**/
+            arr = new int[] {1, 1, 4};
+            Print.p("isSemiPalindrome " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {4, 2, 2};
+            Print.p("isSemiPalindrome " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {2, 4, 2};
+            Print.p("isSemiPalindrome " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {-1, -1};
+            Print.p("isSemiPalindrome " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {-1};
+            Print.p("isSemiPalindrome " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            arr = new int[] {};
+            Print.p("isSemiPalindrome " + Ex13.longestNearlyPal(arr) + ", array=", arr);
+            /**/
+        }
+    }
+}
+/* test numOfPaths
+//p(numOfPaths(1, 1));
+//int n = numOfPaths(SIZE, SIZE);
+//int[] arr = new int[n];
+//numOfPaths(_size, _size);
+//p("Enter 4 numbers, seperated by space for array with 4 numbers");
+ * 
+private static int _size = 2;
+private static int _count = 0;
+private static int _route = 0;
+private static int[] lastX = new int[100], lastY = new int[100];
+private static int numOfPaths(int x, int y, int _route)
+{
+Print.p(x, y, _count);
+if (x == 0 && y == 0) return 0;
+if (x != 0 && y != 0)
+{
+if (p) Print.p("_route=" + _route + ", x=" + 
+x + ", y=" + y + ", lastX=" + lastX[_route] + ", lastY=" + lastY[_route]);
+lastX[_route] = x;
+lastY[_route] = y;
+_route++;
+}
+if (p) Print.p("_route2=" + _route);
+//        if (x == 0 || y == 0) return 1;
+//if (x == _size && y == _size) return 0;
+//if (x < 0 || y < 0 || x > MAX || y > MAX) return 0;
+if (x > 0) 
+{
+if (p) Print.p("calling 1");
+numOfPaths(x - 1, y, _route);    // left
+if (x - 1 == 0 && y == 0) _count++;
+if (p) Print.p("returning 1");
+}
+if (x < _size) 
+{
+if (p) Print.p("before calling 3 _route=" + (_route - 1) + ", x=" + 
+x + ", y=" + y + ", lastX=" + lastX[_route - 1] + ", lastY=" + lastY[_route - 1]);
+if (x != lastX[_route - 1] && y != lastY[_route - 1])
+{
+if (p) Print.p("calling 3");
+numOfPaths(x + 1, y, _route);    // right
+if (x + 1 == 0 && y == 0) _count++;
+if (p) Print.p("returning 3");
+}
+}
+if (y > 0)
+{
+if (p) Print.p("calling 2");
+numOfPaths(x, y - 1, _route);    // down
+if (x == 0 && y - 1 == 0) _count++;
+if (p) Print.p("returning 2");
+
+}
+//if (x < _size) numOfPaths(x + 1, y);  // right
+//if (y < MAX) numOfPaths(x, y + 1);    // up
+Print.p("_count = " + _count);
+return 0;
+}
+private static int numOfPathsOrg(int x, int y)
+{
+if (x == 0 && y == 0) return 0;
+if (x == 0 || y == 0) return 1;
+return numOfPathsOrg(x - 1, y) + numOfPathsOrg(x, y - 1);
+}
+ */
